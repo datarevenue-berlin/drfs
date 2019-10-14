d0befd2517a5eea654502c45f819751cae39dc80
michcio1234 <michcio1234@gmail.com>
2019-10-14 09:57:05 +0200

    Save storage options in RemotePath objects.
    Allow setting them on instantiation.

--- drfs/path.py
+++ drfs/path.py
@@ -1,4 +1,5 @@
 import urllib.parse
+import warnings
 from pathlib import Path, PurePath
 
 from urlpath import URL, cached_property
@@ -11,6 +12,7 @@ from drfs.filesystem import FILESYSTEMS, get_fs
 PATH_CLASS = type(Path())
 
 
+# noinspection PyUnresolvedReferences
 class DRPathMixin:
     @property
     def istemplate(self):
@@ -27,14 +29,28 @@ class DRPathMixin:
     
     def format(self, *args, **kwargs):
         return DRPath(str(self).format(*args, **kwargs))
+    
+    @property
+    def storage_options(self):
+        try:
+            opts = self._storage_options
+        except AttributeError as e:
+            warnings.warn(str(e))
+            return None
+        if opts is not None:
+            return opts
+        return settings.FS_OPTS
+    
+    opts = storage_options
 
 
 class RemotePath(URL, DRPathMixin):
     """A very pathlib.Path version for RemotePaths."""
 
-    def __new__(cls, *args, **kwargs):
+    def __new__(cls, *args, storage_options=None, **kwargs):
         self = cls._from_parts(args, init=False)
         self._init()
+        self._storage_options = storage_options
         return self
 
     def _init(self):
@@ -45,7 +61,7 @@ class RemotePath(URL, DRPathMixin):
     def _accessor(self):
         if self._acc_real is None:
             try:
-                self._acc_real = FILESYSTEMS[self.scheme](**settings.FS_OPTS)
+                self._acc_real = FILESYSTEMS[self.scheme](**self.opts)
             except KeyError:
                 raise ValueError('Scheme {} not found in available filesystems'
                                  ', try installing it.'.format(self.scheme))
@@ -97,6 +113,11 @@ class RemotePath(URL, DRPathMixin):
         return self._root \
                + self._flavour.sep.join(urllib.parse.quote(i, safe=safe_pchars) for i in self._parts[begin:-1] + [self.name]) \
                + self.trailing_sep
+    
+    def _make_child(self, args):
+        res = super()._make_child(args)
+        res._storage_options = self._storage_options
+        return res
 
 
 class LocalPath(PATH_CLASS, DRPathMixin):
