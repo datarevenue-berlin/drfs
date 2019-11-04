d57decbe7044685c210381c8750086cc4de5552f
Alan Höng <alan.f.hoeng@gmail.com>
2019-11-04 21:32:57 +0100

    Use ADLFS with store name prefix to mimic s3 api.
    ADL paths are expected now to contain the store name like:
    
    adl://somestore/path/to/file.txt

--- drfs/filesystems/azure_datalake.py
+++ drfs/filesystems/azure_datalake.py
@@ -1,27 +1,84 @@
 from azure.datalake.store import lib, AzureDLFileSystem
-
 from drfs.filesystems.base import FileSystemBase, FILESYSTEMS
 
 
 class AzureDataLakeFileSystem(FileSystemBase):
     fs_cls = AzureDLFileSystem
-    scheme = 'adl'
+    scheme = "adl"
     is_remote = True
     supports_scheme = False
 
-    def __init__(self, tenant_id=None, client_id=None, client_secret=None,
-                 **kwargs):
+    def __init__(self, tenant_id=None, client_id=None, client_secret=None, **kwargs):
         self.tenant_id = tenant_id
         self.client_id = client_id
         self.client_secret = client_secret
-        self.store_name = kwargs['host']
         self.kwargs = kwargs
-        self.kwargs['store_name'] = kwargs['host']
-        token = lib.auth(tenant_id=self.tenant_id,
-                         client_id=self.client_id,
-                         client_secret=self.client_secret)
-        self.kwargs['token'] = token
+        # self.kwargs['store_name'] = kwargs['host']
+        token = lib.auth(
+            tenant_id=self.tenant_id,
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+        )
+        self.kwargs["token"] = token
         self.fs = AzureDLFileSystem(**self.kwargs)
+
+    def _parse_store_name(self, path):
+        from drfs.path import RemotePath
+
+        if not isinstance(path, RemotePath):
+            path = RemotePath(path)
+
+        store_name, path = path.hostname, path.path
+        if store_name == "":
+            raise ValueError(
+                "Can't connect without store name. Please provide the path in the "
+                "following form: 'adl://STORE_NAME/folder/file.extension'!"
+            )
+
+    def _connect(self, path):
+        self.fs.kwargs["store_name"], path = self._parse_store_name(path)
+        self.fs.connect()
+        return path
+
+    def ls(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).ls(path, *args, **kwargs)
+
+    def open(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).open(path, *args, **kwargs)
+
+    def exists(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).exists(path, *args, **kwargs)
+
+    def remove(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).remove(path, *args, **kwargs)
+
+    def mv(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).mv(path, *args, **kwargs)
+
+    def makedirs(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).makedirs(path, *args, **kwargs)
+
+    def rmdir(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).rmdir(path, *args, **kwargs)
+
+    def info(self, path, *args, **kwargs):
+        path = self._connect(path)
+        return super(AzureDataLakeFileSystem, self).info(path, *args, **kwargs)
+
+    def walk(self, *args, **kwargs):
+        arg0 = self._connect(args[0])
+        return super(AzureDataLakeFileSystem, self).walk(arg0, *args[1:], **kwargs)
+
+    def glob(self, *args, **kwargs):
+        arg0 = self._connect(args[0])
+        return super(AzureDataLakeFileSystem, self).glob(arg0, *args[1:], **kwargs)
 
 
 FILESYSTEMS[AzureDataLakeFileSystem.scheme] = AzureDataLakeFileSystem
